d580750489ac75b9a811bca68f71ccd8c986fd8a
Diounata <jonathamcordeiro42@gmail.com>
2022-12-22 12:27:52 -0300

    fix: updateTransactionCards

---
+++ client/src/contexts/TransactionContext/updateTransactionCards.ts
@@ -0,0 +1,43 @@
+import { TransactionProps, TransactionCardsProps } from './types'
+
+interface UpdateTransactionCardsProps {
+  transactionCards: TransactionCardsProps
+  transactions: TransactionProps[]
+  transaction?: TransactionProps
+  oldTransaction?: TransactionProps
+}
+
+function findLastAdditionDate(transactions: TransactionProps[], type?: 'income' | 'outcome') {
+  return type
+    ? transactions.find(transaction => transaction.type === type)?.date ?? 0
+    : transactions[0]?.date ?? 0
+}
+
+export function updateTransactionCards({
+  transactionCards,
+  transactions,
+  transaction,
+  oldTransaction,
+}: UpdateTransactionCardsProps) {
+  if (oldTransaction) {
+    const { price, type } = oldTransaction
+
+    transactionCards[type].value -= price
+    transactionCards.total.value -= type === 'income' ? price : price * -1
+
+    transactionCards[type].lastAddition = findLastAdditionDate(transactions, type)
+    transactionCards.total.lastAddition = findLastAdditionDate(transactions)
+  }
+
+  if (transaction) {
+    const { price, type } = transaction
+
+    transactionCards[type].value += price
+    transactionCards.total.value = transactionCards.income.value - transactionCards.outcome.value
+
+    transactionCards[type].lastAddition = findLastAdditionDate(transactions, type)
+    transactionCards.total.lastAddition = findLastAdditionDate(transactions)
+  }
+
+  return transactionCards
+}
