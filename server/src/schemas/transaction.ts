c54a197e7da53009f5090b5996e0d3d02defdaac
Diounata <jonathamcordeiro42@gmail.com>
2023-01-11 11:59:31 -0300

    fix: Transaction schema

--- server/src/schemas/transaction.ts
+++ server/src/schemas/transaction.ts
@@ -1,5 +1,11 @@
 import { model, Schema } from 'mongoose'
 
-const transcationSchema = new Schema({ name: String })
+const transcationSchema = new Schema({
+  title: { type: String, required: true },
+  price: { type: Number, required: true, min: 0.01 },
+  type: { type: String, required: true, lowercase: true },
+  category: { type: String, required: true },
+  date: { type: Number, default: () => Date.now() },
+})
 
 export const Transactions = model('transactions', transcationSchema)
